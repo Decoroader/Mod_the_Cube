f6094f123b5dd469c3527d7bda02a3f91ebb7596
Decoroader <secbittree@gmail.com>
2020-12-26 00:51:21 +0200

    3rd edition for Mod the Cube Lab
    rotation + color (up/down Arrows), move on X (left/right Arrows), space - balls, transparent doesn't work

---
+++ Assets/ModTheCube/BallShot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BallShot : MonoBehaviour
+{
+    public GameObject mainCube;
+    public GameObject ballPrefab;
+
+    private GameObject currBall;
+    private float speed = 33.0f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currBall = Instantiate(ballPrefab, mainCube.transform.position, mainCube.transform.rotation) as GameObject;
+            currBall.transform.localScale = Vector3.one * Random.Range(1.0f, 3.0f);
+            currBall.GetComponent<Renderer>().material.color = 
+                new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            Destroy(currBall, 1.5f);
+        }
+        if(currBall != null)
+            currBall.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,69 +5,97 @@ public class Cube : MonoBehaviour
     public MeshRenderer Renderer;
 
     private float speedRotation;
+    private float speedMove = 1.10f;
     private float rangeX = 1.0f;
     private float rangeY_Z = 5.0f;
-    private float startScale;
-    private int dinamicScale = 100;
-    [SerializeField] private int additionalParameter = 0;
+
+    private float currentScale;
+    private float scaleDiscret = 0.055f;
+    private int scaleAmass = 1;
+
+    private int mainParameter;
+
     private Material material;
+    private float rad = 57.29578f;
+    private float horizontal;
+    private int witeThreshold = 500;
+    private float piNumb = Mathf.PI;
 
     void Start()
     {
         transform.position = new Vector3(Random.Range(-rangeX, rangeX),
             Random.Range(-rangeY_Z, rangeY_Z), Random.Range(-rangeY_Z, rangeY_Z));
 
-        startScale = Random.Range(1.0f, 5.0f);
+        currentScale = Random.Range(1.0f, 5.0f);
+        transform.localScale = Vector3.one * currentScale;
         
-        material = Renderer.material;
-        material.color = Random.ColorHSV();
+        material = Renderer.material; 
 
-        speedRotation = Random.Range(11.0f, 33.0f);
+        speedRotation = 1.5f;
+        mainParameter = Random.Range(1, 91);
     }
     
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        // this line doesn't work the way i want:
+        //transform.Translate(Vector3.right * Time.deltaTime * horizontal * speedMove);
+        transform.position = new Vector3(transform.position.x + horizontal * speedMove, 
+            transform.position.y, transform.position.z);
+
         if (Input.GetKey(KeyCode.UpArrow))
-            additionalParameter++;
+            mainParameter++;
         if (Input.GetKey(KeyCode.DownArrow))
-            additionalParameter--;
-        transform.Rotate(speedRotation * Time.deltaTime + additionalParameter, 0.5f, 0.5f);
-        
-        dinamicScale++;
+            mainParameter--;
+        transform.Rotate(speedRotation * Time.deltaTime * mainParameter, 
+            speedRotation * Time.deltaTime * mainParameter, speedRotation * Time.deltaTime * mainParameter);
 
-        transform.localScale = Vector3.one * startScale * dinamicScale/100;
-        Coloring(additionalParameter);
+        Coloring(mainParameter);
 
-        
+        ScalingCube();
+
+        if (Input.GetKey(KeyCode.Q))
+            Application.Quit();
     }
-    private void Coloring(int addParameter)
+    private void ScalingCube()
+	{
+        if ((transform.localScale.x > 7) || (transform.localScale.x < 1))
+            scaleAmass *= -1;
+
+        currentScale += scaleDiscret * scaleAmass;
+        transform.localScale = Vector3.one * currentScale;
+    }
+    private void Coloring(int colorControl)
     {
-        if (transform.localScale.x > 7)
+        if (colorControl < 0)
+            colorControl *= -1;
+
+        float sectionR = colorControl / rad;
+        float sectionG = 0;
+        float sectionB = 0;
+        float sectionA = .01f;
+
+        if ((colorControl >= 45))
+            sectionG = (colorControl / rad - piNumb / 4) * 2;
+        if (colorControl >= 90)
+            sectionB = (colorControl / rad - piNumb / 2);
+        if ((colorControl < witeThreshold)&&(colorControl >= 180))
+            sectionA = (float)180 / (colorControl * 1);
+        else if (colorControl >= witeThreshold)
         {
-            dinamicScale = 10;
-
-            material.color = Random.ColorHSV();
-
-            material.color = new Color(Random.Range(0.1f, 1.0f), Random.Range(0.1f, 1.0f), Random.Range(0.1f, 1.0f),
-                0.9f);
-
+            sectionA = 1;
+            sectionR = 0;
+            sectionG = piNumb / 2;
+            sectionB = piNumb / 2;
         }
-        if (addParameter < 0)
-            addParameter *= -1;
-        if (addParameter > 90)
-            addParameter = 90;
 
-        float trigArg = (float)(addParameter / 57.29578);
-        material.color = new Color((float)(System.Math.Cos(trigArg)), 
-            material.color.g * (float)(System.Math.Sin(trigArg * 2)),
-            (float)System.Math.Sin(trigArg));
+        material.color = new Color((Mathf.Cos(sectionR)), (Mathf.Sin(sectionG)),
+            Mathf.Sin(sectionB), sectionA);
 
         if (Input.GetKey(KeyCode.C))
         {
-            Debug.Log("Sin addParameter is " + (System.Math.Sin(addParameter / 57.29578)));
-            Debug.Log("Cos addParameter is " + (System.Math.Cos(addParameter / 57.29578)));
             Debug.Log("material.color.r " + material.color.r + ", material.color.g " + material.color.g +
-                ", material.color.b " + material.color.b);
+                ", material.color.b " + material.color.b + ", material.color.a " + material.color.a);
         }
     }
 }
